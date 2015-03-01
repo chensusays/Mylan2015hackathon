56d6497b1a3d57ddc6f8263de74b0562ab723492
Zachary Oscar Ovington <zoo1@pitt.edu>
2015-02-28 21:51:30 -0500

    generic views

--- routes/views.js
+++ routes/views.js
@@ -18,17 +18,35 @@ router.delete('/:user', function(req, res, next) {
 });
 
 /* GET view views*/
-router.post('/', function(req, res, next) {
-
+router.get('/', function(req, res, next) {
+view.find(null, 'name -_id',
+  {
+    skip:0 // Starting Row
+  },
+    function (err, scores) {
+      if (err) return next(err);
+        res.json(scores);
+  });
 });
+
 /* POST create new view*/
 router.post('/', function(req, res, next) {
-
+  if(!req.body.name)
+    return res.json({"status":"view name not defined"});
+  view.create({"name" : req.body.name,
+                "drugs" : []}, function (err, post) {
+    if (err) return next(err);
+      res.json( {"status": "success" });
+  });
 });
 
 /* DELETE delete view*/
 router.delete('/',function(req, res, next) {
-
+  if(!req.body.name)
+    return res.json({"status":"view name not defined"});
+  view.find({ name : req.body.name }).remove( function (err, post) {
+    if (err) return next(err);
+      res.json( {"status": "success" }); });
 });
 
 module.exports = router;
